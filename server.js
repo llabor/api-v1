8a4e9e559bd1ce76d44d02c50ff178843f69a55e
Angel Rayo <angelmrayo@gmail.com>
2018-02-19 18:53:06 +0100

    Versión con MLab (GET Y PUT)

--- server.js
+++ server.js
@@ -1,22 +1,82 @@
-
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
+
+var requestJson = require('request-json')
+
 app.use(bodyParser.json())
+
+
+
+var urlMlabRaiz = "https://api.mlab.com/api/1/databases/bdbancoar/collections"
+var apiKey = "apiKey=50c5ea68e4b0a97d668bc84a"
+var clienteMlab
+
+
+app.get('/apitechu/v5/usuarios', function(req, res) {
+    clienteMlab = requestJson.createClient(urlMlabRaiz + "/usuarios?" + apiKey)
+    clienteMlab.get('', function(err, resM, body) {
+      if (!err) {
+        res.send(body)
+      }
+    })
+})
+
+app.get('/apitechu/v5/usuarios/:id', function(req, res) {
+    var id = req.params.id
+    var query = 'q={"id":' + id + '}&f={"nombre":1, "apellidos":1, "_id":0}'
+    clienteMlab = requestJson.createClient(urlMlabRaiz + "/usuarios?" + query + "&l=1&" + apiKey)
+    clienteMlab.get('', function(err, resM, body) {
+      if (!err) {
+        if (body.length > 0)
+          res.send(body[0])
+          //res.send({"nombre":body[0].nombre, "apellidos":body[0].apellidos})
+        else {
+          res.status(404).send('Usuario no encontrado')
+        }
+      }
+    })
+})
+
+app.post('/apitechu/v5/login', function(req, res) {
+    var email = req.headers.email
+    var password = req.headers.password
+    var query = 'q={"email":"' + email + '","password":"' + password + '"}'
+    clienteMlab = requestJson.createClient(urlMlabRaiz + "/usuarios?" + query + "&l=1&" + apiKey)
+    clienteMlab.get('', function(err, resM, body) {
+      if (!err) {
+        if (body.length == 1) // Login ok
+        {
+          clienteMlab = requestJson.createClient(urlMlabRaiz + "/usuarios")
+          var cambio = '{"$set":{"logged":true}}'
+          clienteMlab.put('?q={"id": ' + body[0].id + '}&' + apiKey, JSON.parse(cambio), function(errP, resP, bodyP) {
+              res.send({"login":"ok", "id":body[0].id, "nombre":body[0].nombre, "apellidos":body[0].apellidos})
+          })
+
+        }
+        else {
+          res.status(404).send('Usuario no encontrado')
+        }
+      }
+    })
+})
+
 
 
 
 var port = process.env.PORT || 3000
 var fs = require('fs')
 
-
-var usuarios = require('./usuarios.json')
-
-console.log("Hola mundo")
-
 app.listen(port)
 
 console.log("API escuchando en el puerto" + port)
+
+
+var usuarios = require('./usuarios.json')
+
+//console.log("Hola mundo")
+
+
 
 app.get('/apitechu/v1', function(req, res)
 {
